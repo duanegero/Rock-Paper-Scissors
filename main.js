07c66fdb70df65c44e6a5fecb8531b8cd77ee28d
duanegero <duanegero11@gmail.com>
2024-06-26 19:58:07 -0300

    Adding rock paper scissors game to git hub

---
+++ main.js
@@ -0,0 +1,34 @@
+function playGame(){
+    let cpuChoiceNumber = Math.floor(Math.random() * 3) + 1;
+    let userChoice = document.getElementById('userChoice').value;
+    console.log(userChoice);
+    
+    let cpuChoice = '';
+    switch(cpuChoiceNumber){
+        case 1:
+            cpuChoice = 'rock';
+            break;
+        case 2:
+            cpuChoice = 'paper';
+            break;
+        case 3:
+            cpuChoice = 'scissors';
+            break;
+    }
+
+    console.log(cpuChoice);
+    
+    if(userChoice == cpuChoice){
+        console.log('TIE');
+        document.getElementById('cpuChoice').innerHTML = `CPU choice ${cpuChoice}`
+        document.getElementById('winner').innerHTML = `TIE`
+    }else if((userChoice == 'rock' && cpuChoice == 'scissors') || (userChoice == 'scissors' && cpuChoice == 'paper') || (userChoice == 'paper' && cpuChoice == 'rock')){
+        console.log('USER WINS');
+        document.getElementById('cpuChoice').innerHTML = `CPU choice ${cpuChoice}`;
+        document.getElementById('winner').innerHTML = `YOU WIN`
+    }else{
+        console.log('CPU WINS');
+        document.getElementById('cpuChoice').innerHTML = `CPU choice ${cpuChoice}`
+        document.getElementById('winner').innerHTML = "CPU";
+    }
+}
